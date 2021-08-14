73449ae4b43635151e4ac8fb7c20e1c68bde5d00
RhysSharpe <rhysgsharpe@gmail.com>
2021-08-14 22:32:09 +0100

    Following the first tutorial
    11 pages completed from TutorialPoints

---
+++ MVVMDemos/TutorialsPointDemo/ViewModel/StudentViewModel.cs
@@ -0,0 +1,64 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Collections.ObjectModel;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using TutorialsPointDemo.Model;
+
+namespace TutorialsPointDemo.ViewModel
+{
+    public class StudentViewModel
+    {
+        public ObservableCollection<Student> Students
+        {
+            get;
+            set;
+        }
+
+        public MyICommand DeleteCommand { get; set; }
+
+        public Student SelectedStudent
+        {
+            get
+            {
+                return _selectedStudent;
+            }
+
+            set
+            {
+                _selectedStudent = value;
+                DeleteCommand.RaiseCanExecuteChanged();
+            }
+        }
+
+        private Student _selectedStudent;
+
+        public StudentViewModel()
+        {
+            LoadStudents();
+            DeleteCommand = new MyICommand(OnDelete, CanDelete);
+        }
+
+        private void OnDelete()
+        {
+            Students.Remove(SelectedStudent);
+        }
+
+        private bool CanDelete()
+        {
+            return SelectedStudent != null;
+        }
+
+        public void LoadStudents()
+        {
+            ObservableCollection<Student> students = new ObservableCollection<Student>();
+
+            students.Add(new Student { FirstName = "Mark", LastName = "Allain" });
+            students.Add(new Student { FirstName = "Allen", LastName = "Brown" });
+            students.Add(new Student { FirstName = "Linda", LastName = "Hamerski" });
+
+            Students = students;
+        }
+    }
+}
